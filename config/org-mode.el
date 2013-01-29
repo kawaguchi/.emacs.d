f833383254c7833867d736bc59cc40d796158dae
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-01-29 16:27:50 +0900

    update org-mode config

--- config/org-mode.el
+++ config/org-mode.el
@@ -1,9 +1,12 @@
+(setq org-agenda-files (list (concat (file-name-as-directory org-directory) "gtd.org")))
+(define-key global-map (kbd "C-c a") 'org-agenda)
+
+;; http://orgmode.org/manual/Template-expansion.html#Template-expansion
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/.org/todo.org" "Tasks")
-         "* TODO %?n %in %a")
-        ("j" "Journal" entry (file+datetree "~/.org/journal.org")
-         "* %?n %Un %in %a")
-        ("n" "Note" entry (file+headline "~/.org/notes.org" "Notes")
-         "* %?n %Un %i")
-        ))
+      '(("t" "Todo" entry (file+headline "gtd.org" "Inbox")
+         "* TODO %?\n  %i\n\n  %U\n  %a")
+        ("n" "Note" entry (file+headline "gtd.org" "Inbox")
+         "* %?\n\n  %U\n  %i")
+        ("j" "Journal" entry (file+datetree "journal.org")
+         "* %?\n  %i\n\n  %U\n  %a")))
 (define-key global-map (kbd "C-c c") 'org-capture)
