a6d9957d96abd7e58094e423a505323b8fb2a308
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-01-31 18:11:52 +0900

    update org-mode config

--- config/org-mode.el
+++ config/org-mode.el
@@ -1,14 +1,21 @@
-(setq org-agenda-files (list (concat (file-name-as-directory org-directory) "gtd.org")))
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (local-unset-key (kbd "C-,"))))
+
+(defun open-org-default-notes-file ()
+  (interactive)
+  (find-file org-default-notes-file))
+(global-set-key (kbd "<f5>") 'open-org-default-notes-file)
+
+(setq org-agenda-files (list org-default-notes-file))
 (define-key global-map (kbd "C-c a") 'org-agenda)
 
 ;; http://orgmode.org/manual/Template-expansion.html#Template-expansion
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "gtd.org" "Inbox")
+      '(("t" "Todo" entry (file+headline org-default-notes-file "Inbox")
          "* TODO %?\n  %i\n\n  %U\n  %a")
-        ("n" "Note" entry (file+headline "gtd.org" "Inbox")
-         "* %?\n\n  %U\n  %i")
-        ("j" "Journal" entry (file+datetree "journal.org")
-         "* %?\n  %i\n\n  %U\n  %a")))
+        ("n" "Note" entry (file+headline org-default-notes-file "Inbox")
+         "* %?\n\n  %U\n  %i")))
 (define-key global-map (kbd "C-c c") 'org-capture)
 
 ;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-ledger.html
