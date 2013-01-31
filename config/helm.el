93af4696716ed1cd2e6ac0ac9367985f07d201a6
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-01-31 13:57:00 +0900

    fix helm config eval-after-loadのformがload前にも呼ばれてるっぽい

--- config/helm.el
+++ config/helm.el
@@ -1,4 +1,4 @@
 (global-set-key (kbd "C-;") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (eval-after-load "helm"
-  '(define-key helm-map (kbd "C-h") 'delete-backward-char))
+  '(if (boundp 'helm-map) (define-key helm-map (kbd "C-h") 'delete-backward-char)))
