e2257e6bdc7a8316498033ff8d8b298ccff29571
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-02-01 14:33:07 +0900

    haskell support first step

---
+++ config/haskell-mode.el
@@ -0,0 +1,7 @@
+(require 'haskell-cabal)
+(setq haskell-program-name "ghci")
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
