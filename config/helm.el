2635aaa9d177b3e26d56f785016f2db3721e0d44
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-02-21 11:41:02 +0900

    update helm-config

--- config/helm.el
+++ config/helm.el
@@ -1,4 +1,8 @@
 (global-set-key (kbd "C-;") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (eval-after-load "helm"
-  '(if (boundp 'helm-map) (define-key helm-map (kbd "C-h") 'delete-backward-char)))
+  '(progn
+     (if (boundp 'helm-map)
+         (define-key helm-map (kbd "C-h") 'delete-backward-char))
+     (if (boundp 'helm-find-files-map)
+         (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char))))
