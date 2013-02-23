e0b988e1934507e9c928a16db515267847478838
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-02-23 19:54:48 +0900

    add scheme config

---
+++ config/scheme.el
@@ -0,0 +1,4 @@
+(add-to-list 'process-coding-system-alist '("gosh" utf-8 . utf-8))
+(setq scheme-program-name "gosh -i")
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
