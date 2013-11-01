8994e06dc2edb12d20c96317c35e1cb9611ff25d
Masaya Kawaguchi <masayakawaguchi@gmail.com>
2013-11-01 12:18:53 +0900

    Setup skk use skkserv

--- config/skk.el
+++ config/skk.el
@@ -1,10 +1,13 @@
 (setq skk-preload t
+      skk-jisyo-code 'utf-8
       default-input-method "japanese-skk"
       skk-henkan-strict-okuri-precedence t
       skk-check-okurigana-on-touroku t
       skk-kakutei-key (kbd "C-\'")
       skk-egg-like-newline t
-      skk-server-portnum 1178)
+      skk-server-host "127.0.0.1"
+      skk-server-portnum 1178
+      skk-jisyo (concat (getenv "HOME") "/Dropbox/skk/skk-jisyo.utf8"))
 
 (mapc (lambda (command)
         (let ((hook (intern (concat (symbol-name command)
